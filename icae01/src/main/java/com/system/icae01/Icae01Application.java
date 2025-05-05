9e6f8fa963cd5e6fe59441f50f44237ce2a2b038
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-05 17:25:17 +0530

    feat:Add initial project structure

---
+++ icae01/src/main/java/com/system/icae01/Icae01Application.java
@@ -0,0 +1,13 @@
+package com.system.icae01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Icae01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Icae01Application.class, args);
+	}
+
+}
---
+++ icae01/src/test/java/com/system/icae01/Icae01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.icae01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Icae01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
